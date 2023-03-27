36b60d690e4df096e98fa2f6c2c8a5e30d78fd06
AlexisDupont2 <816522@mystma.org>
2023-03-27 14:19:39 -0500

    2

--- main.py
+++ main.py
@@ -3,6 +3,6 @@ def password(p):
     if (p == "Knights19"):
         return ("ACCESS GRANTED")
     else:
-        return ("ACCESS DENINED")
+        return ("ACCESS DENIED")
 password("password") 
 password("Knights19") 
